86b20695a52750b7febe7866bfea45b56dbcc309
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-11-30 16:38:31 -0500

    Updated js to include functions for async and fetch repos, and to display repos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const overview = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list")
 const username = "wintert98";
-
+/*-------- Async function to call API and get user data  -----------*/
 const githubProfile = async function () {
     const res = await fetch(`https://api.github.com/users/${username}`);
     const data = await res.json();
     displayUser(data);
-    console.log(data);
 };
 
 githubProfile();
 
+/*--------  Function to display user data  -----------*/
 const displayUser = function (data) {
    let div = document.createElement("div");
    div.classList.add("user-info");
@@ -24,4 +25,24 @@ const displayUser = function (data) {
    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
  </div>`
  overview.append(div);
+};
+
+/*-------- Async function to call API and get repo data  -----------*/
+const githubRepo = async function () {
+    const res = await fetch(`https://api.github.com/users/${username}/repos`);
+    const data = await res.json();
+    displayRepo(data);
+};
+githubRepo();
+/*--------  Function to display repo data  -----------*/
+const displayRepo = function (data) {
+
+    for(let i = 0; i < data.length; i++) {
+        let li = document.createElement('li')
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${data[i].name}</h3>`;
+        repoList.append(li);
+
+    }
+
 };
